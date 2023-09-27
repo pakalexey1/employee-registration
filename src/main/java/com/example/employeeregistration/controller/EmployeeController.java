f1b6dd5717babcec1a41986542f6a44b77a25cf6
pakalexey <mye604@gmail.com>
2023-09-27 17:11:46 -0400

    Project finished

--- src/main/java/com/example/employeeregistration/controller/EmployeeController.java
+++ src/main/java/com/example/employeeregistration/controller/EmployeeController.java
@@ -1,10 +1,17 @@
 package com.example.employeeregistration.controller;
 
+import com.example.employeeregistration.bootstrap.DataGenerator;
 import com.example.employeeregistration.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import javax.xml.crypto.Data;
 
 @Controller
 @RequestMapping("/employee")
@@ -13,7 +20,29 @@ public class EmployeeController {
     @GetMapping("/register")
     public String createEmployee(Model model){
 
-        model.addAttribute("", new Employee());
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("states", DataGenerator.getAllState());
+
         return "employee/employee-create";
+
+    }
+
+    @PostMapping("/list")
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){
+
+            model.addAttribute("states", DataGenerator.getAllState());
+
+            return "employee/employee-create";
+        }
+
+        DataGenerator.saveEmployee(employee);
+
+        model.addAttribute("employees", DataGenerator.readAllEmployees());
+
+
+
+        return "employee/employee-list";
     }
 }
--- src/main/java/com/example/employeeregistration/model/Employee.java
+++ src/main/java/com/example/employeeregistration/model/Employee.java
@@ -1,7 +1,12 @@
 package com.example.employeeregistration.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -11,8 +16,16 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+//    @NotNull //accepts everything besides null, e.g. "" is acceptable
+//    @NotEmpty //same as null plus doesn't accept "", e.g. " " is acceptable
+//    @NotBlank //same as empty plus doesn't accept " "
+
+    @NotBlank
+    @Size(max=12, min=2)
     private String firstName;
     private String lastName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String email;
     private String password;
